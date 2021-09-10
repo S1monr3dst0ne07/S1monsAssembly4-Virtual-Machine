4e02088d4a9ad9acc459106460f2f66187b5c633
S1mon <jaforsi.kozak@gmail.com>
2021-09-10 17:39:22 +0200

    all commands running

--- S1monsAssembly4 Virtual Machine/Main.c
+++ S1monsAssembly4 Virtual Machine/Main.c
@@ -3,8 +3,11 @@
 #include <string.h>
 #include <errno.h>
 
-#define FILE_BUFFER_SiZE 256
-#define MAPPER_SIZE      2048
+#define FILE_BUFFER_SiZE    256
+#define MAPPER_SIZE         2048
+#define MEM_SIZE            65535
+#define STACK_SIZE          2048
+#define ATTR_SIZE           256
 
 #define ArraySize(x) sizeof(x) / sizeof(x[0])
 
@@ -12,8 +15,11 @@
 
 typedef struct 
 {
+    char* instOrg;
+    char* attrOrg;
+
     int inst;
-    char* attr;
+    char attr[ATTR_SIZE];
     int lineNum;
     int conLine;
 
@@ -62,16 +68,18 @@ enum
 
 
 
-int findIndex(const char* sourceArray[], const char* targetChar)
+int findIndex(const char* sourceArray[], const char* targetChar, const int size)
 {
-    for (int i = 0; i < ArraySize(sourceArray); i++)
+    for (int i = 0; i < size; i++)
     {
-        char* currentIndex = sourceArray[i];
-        if (strcmp(currentIndex, targetChar) != 0) return i;
-
+        const char* currentIndex = sourceArray[i];
+        if (strcmp(currentIndex, targetChar) == 0)
+        {
+            return i;
+        }
     }
 
-    return NULL;
+    return -1;
 }
 
 
@@ -118,34 +126,58 @@ int main(int argc, char** argv)
 
     //parse file into a list of structs
     int lineIndex = 0;
-    int attrBufferIndex = 0;
+    int mapperIndex = 0;
     char *inst, *attr, *attrNew;
-    char* attrMapper[MAPPER_SIZE];
-    int lineMapper[MAPPER_SIZE];
+    char* attrMapper[MAPPER_SIZE] = { 0 };
+    int lineMapper[MAPPER_SIZE]   = { 0 };
 
     while (fgets(buffer, FILE_BUFFER_SiZE - 1, filePtr))
     {
-        buffer[strcspn(buffer, "\n")] = 0;
+        buffer[strcspn(buffer, "\n")] = NULL;
         if (buffer[0] != 0 && buffer[0] != '"')
         {
+            lineBuffer[lineIndex].conLine = NULL;
+
             inst = strtok(buffer, " ");
             attr = strtok(NULL, " ");
 
+            //DEBUG START
+            /*
+            char* attrOrg;
+            char* instOrg = malloc(sizeof(inst) + sizeof(char));
+            memset(instOrg, 0, sizeof(instOrg));
+            strcpy(instOrg, inst);
+
             if (attr != NULL)
             {
-                attrNew = malloc(sizeof(attr));
-                strcpy(attrNew, attr, sizeof(attr));
+                attrOrg = malloc(sizeof(attr) + sizeof(char));
+                memset(attrOrg, 0, sizeof(attrOrg));
+                strcpy(attrOrg, attr);
             }
             else
             {
-                attrNew = NULL;
+                attrOrg = NULL;
             }
 
+            lineBuffer[lineIndex].instOrg = instOrg;
+            lineBuffer[lineIndex].attrOrg = attrOrg;
+            */
+            //DEBUG END
+
+            if (attr != NULL)
+            {
+                strcpy(lineBuffer[lineIndex].attr, attr);
+            }
+            else
+            {
+                memset(lineBuffer[lineIndex].attr, 0, ATTR_SIZE);
+            }
 
 
 
             int instNum = NULL;
-            int index, targetLine;
+            int index = 0;
+            int targetLine = 0;
 
             /**/ if (strcmp(inst, "set") == 0)      instNum = set;
             else if (strcmp(inst, "add") == 0)      instNum = add;
@@ -170,43 +202,37 @@ int main(int argc, char** argv)
 
             else if (strcmp(inst, "lab") == 0)
             {
-                lineMapper[attrBufferIndex] = lineIndex;
-                attrMapper[attrBufferIndex] = attrNew;
+                lineMapper[mapperIndex] = lineIndex;
+                attrMapper[mapperIndex] = lineBuffer[lineIndex].attr;
                 instNum = lab;
 
-                attrBufferIndex++;
+                mapperIndex++;
             }
             
 
             else if (strcmp(inst, "got") == 0)
             {
-                lineBuffer[lineIndex].conLine = lineMapper[findIndex((const)attrMapper, attrNew)];
                 instNum = got;
             }
             else if (strcmp(inst, "jm0") == 0)
 
             {
-                lineBuffer[lineIndex].conLine = lineMapper[findIndex((const)attrMapper, attrNew)];
                 instNum = jm0;
             }
             else if (strcmp(inst, "jmA") == 0)
             {
-                lineBuffer[lineIndex].conLine = lineMapper[findIndex((const)attrMapper, attrNew)];
                 instNum = jmA;
             }
             else if (strcmp(inst, "jmG") == 0)
             {
-                lineBuffer[lineIndex].conLine = lineMapper[findIndex((const)attrMapper, attrNew)];
                 instNum = jmG;
             }
             else if (strcmp(inst, "jmL") == 0)
             {
-                lineBuffer[lineIndex].conLine = lineMapper[findIndex((const)attrMapper, attrNew)];
                 instNum = jmL;
             }
             else if (strcmp(inst, "jmS") == 0)
             {
-                lineBuffer[lineIndex].conLine = lineMapper[findIndex((const)attrMapper, attrNew)];
                 instNum = jmS;
             }
 
@@ -229,7 +255,6 @@ int main(int argc, char** argv)
 
 
             lineBuffer[lineIndex].inst = instNum;
-            lineBuffer[lineIndex].attr = attrNew;
             lineBuffer[lineIndex].lineNum = lineIndex;
 
 
@@ -237,23 +262,191 @@ int main(int argc, char** argv)
         }
     }
 
+
+
+
+    //set conLine of all lines containing a jump
+
+    int   runInst;
+    char* runAttr;
+
     for (int i = 0; i < lineCount; i++)
     {
-        
-        //printf("%d\n", lineBuffer[i].inst);
-        printf("%s\n", lineBuffer[i].attr);
+        LINE lineStruct = lineBuffer[i];
+        runInst = lineStruct.inst;
+        runAttr = lineStruct.attr;
+
+
+        if (runInst >= got && runInst <= jmS)
+        {
+            int attr2lineIndex = findIndex(attrMapper, runAttr, mapperIndex);
+            lineBuffer[i].conLine = lineMapper[attr2lineIndex];
+
+        }
     }
 
+    //now finally run with lightspeed
+    unsigned short int acc = 0;
+    unsigned short int reg = 0;
 
-    for (int i = 0; i < attrBufferIndex; i++)
+    unsigned short int mem[MEM_SIZE]     = { 0 };
+    unsigned short int stack[STACK_SIZE] = { 0 };
+    int stackPtr = 0;
+
+    char outputBuffer[2] = "";
+
+
+    for (int execPtr = 0; execPtr < lineCount; execPtr++)
     {
+        LINE lineStruct = lineBuffer[execPtr];
 
-        printf("AttrMapper: %s\n", attrMapper[i]);
-        printf("LineMapper: %d\n", lineMapper[i]);
+        runInst = lineStruct.inst;
+        runAttr = lineStruct.attr;
+
+
+        switch (runInst)
+        {
+            case set:
+                reg = (int)atoi(runAttr);
+                break;
+
+            case add:
+                acc += reg;
+                break;
+
+            case sub:
+                acc -= reg;
+                break;
+
+            case shg:
+                acc <<= 1;
+                break;
+
+            case shs:
+                acc >>= 1;
+                break;
+
+            case lor:
+                acc |= reg;
+                break;
+
+            case and:
+                acc &= reg;
+                break;
+
+            case xor:
+                acc ^= reg;
+
+            case not:
+                acc = ~acc;
+
+            case lDA:
+                acc = mem[(int)atoi(runAttr)];
+                break;
+
+            case lDR:
+                reg = mem[(int)atoi(runAttr)];
+                break;
+
+            case sAD:
+                mem[(int)atoi(runAttr)] = acc;
+                break;
+
+            case sRD:
+                mem[(int)atoi(runAttr)] = reg;
+                break;
+
+
+            case lPA:
+                acc = mem[mem[(int)atoi(runAttr)]];
+                break;
+
+            case lPR:
+                reg = mem[mem[(int)atoi(runAttr)]];
+                break;
+
+            case sAP:
+                mem[mem[(int)atoi(runAttr)]] = acc;
+                break;
+
+            case sRP:
+                mem[mem[(int)atoi(runAttr)]] = reg;
+                break;
+
+            case out:
+                printf("%d\n", mem[atoi(runAttr)]);
+                break;
+
+            case inp_asm:
+                printf(">>>");
+                scanf("%hd", &mem[atoi(runAttr)]);
+                break;
+
+
+            case got:
+                execPtr = lineStruct.conLine;
+                break;
+
+            case jm0:
+                if(acc == 0) execPtr = lineStruct.conLine;
+                break;
+
+            case jmA:
+                if (acc == reg) execPtr = lineStruct.conLine;
+                break;
+
+            case jmG:
+                if (acc > reg) execPtr = lineStruct.conLine;
+                break;
+
+            case jmL:
+                if (acc < reg) execPtr = lineStruct.conLine;
+                break;
+
+            case jmS:
+                //save the execPtr on the stack
+                stack[stackPtr++] = execPtr;
+                execPtr = lineStruct.conLine;
+                break;
+
+            case ret:
+                execPtr = stack[--stackPtr];
+                break;
+
+            case pha:
+                stack[stackPtr++] = acc;
+                break;
+
+            case pla:
+                acc = stack[--stackPtr];
+                break;
+
+            case brk:
+                exit(0);
+                break;
+
+            case clr:
+                acc = 0;
+                reg = 0;
+                break;
+
+            case putstr:
+                outputBuffer[0] = (char)acc;
+                printf("%s", &outputBuffer);
+
+            default:
+                break;
+        }
+
+
+
     }
 
 
 
-    printf("Done");
+
     return 0;
 }
+
+
+
